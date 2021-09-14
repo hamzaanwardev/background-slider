c7e3840edeb7a824953db6077c9efe14b42c4591
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-09-15 01:54:40 +0500

    updated JS file

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const body = document.body;
 const slides = document.querySelectorAll('.slide');
 const leftBtn = document.getElementById('left');
 const rightBtn = document.getElementById('right');
+
+let activeSlide = 2;
+    
+setBgToBody ();
+function setBgToBody () {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlide () {
+    slides.forEach (slide => slide.classList.remove('active'));
+    slides[activeSlide].classList.add('active');
+}
+
