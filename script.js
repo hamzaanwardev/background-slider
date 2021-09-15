cd7384558334cded2609b99bb596d42dacde45ec
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-09-15 21:18:50 +0500

    Completed

--- script.js
+++ script.js
@@ -2,16 +2,35 @@ const body = document.body;
 const slides = document.querySelectorAll('.slide');
 const leftBtn = document.getElementById('left');
 const rightBtn = document.getElementById('right');
+const slideContainer = document.getElementById('slide-container');
 
-let activeSlide = 2;
-    
+let activeSlide = 0;
+
+    leftBtn.addEventListener ('click', () => {
+            activeSlide--
+        if (activeSlide < 0) {
+            activeSlide = slides.length;
+        }
+        setBgToBody();
+        setActiveSlide();
+    });
+
+    rightBtn.addEventListener ('click', () =>  {
+        activeSlide++;
+        if (activeSlide > slides.length - 1) {
+            activeSlide = 0;
+        }
+        setBgToBody();
+        setActiveSlide();
+    });
 setBgToBody ();
 function setBgToBody () {
     body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+    slideContainer.style.backgroundImage = slides[activeSlide].style.backgroundImage;
 }
 
 function setActiveSlide () {
-    slides.forEach (slide => slide.classList.remove('active'));
+    slides.forEach ((slide) => slide.classList.remove('active'));
     slides[activeSlide].classList.add('active');
 }
 
